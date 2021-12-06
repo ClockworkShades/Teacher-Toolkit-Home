2682dab3981ff291c0b955763b928dc9fd4b62d1
Toby Gumm <18gummt@utcsheffield.org.uk>
2021-12-06 13:26:55 +0000

    Updated

--- Teacher Toolkit Home/Form2.Designer.cs
+++ Teacher Toolkit Home/Form2.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -46,12 +49,45 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(181, 134);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(59, 150);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 45);
+            this.button1.Size = new System.Drawing.Size(126, 45);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Timer";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(233, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stopwatch";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(233, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 90);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Student Picker";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(59, 218);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 90);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Dice";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
@@ -59,6 +95,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(485, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
@@ -73,5 +112,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
